d42447fef5f9eca6a464334832302f0169718816
Milena Duarte <milena.duarte@somosicev.com>
2025-02-10 13:04:30 -0300

    texto

--- src/components/features/FeaturesCards.tsx
+++ src/components/features/FeaturesCards.tsx
@@ -2,13 +2,7 @@
 
 import { IconUser } from "@tabler/icons-react";
 import { GraduationCap, Clover } from "lucide-react";
-import {
-  Card,
-  Container,
-  SimpleGrid,
-  Text,
-  Title,
-} from "@mantine/core";
+import { Card, Container, SimpleGrid, Text, Title } from "@mantine/core";
 import classes from "./FeaturesCards.module.css";
 
 const mockdata = [
@@ -33,31 +27,35 @@ const mockdata = [
 ];
 
 export function FeaturesCards() {
-
   return (
     <Container size="lg">
-
       <Title order={2} className={classes.title} ta="center" mt="sm">
         O que é a Binatti Community?
       </Title>
 
       <Text c="dimmed" className={classes.description} ta="center" mt="md">
-        Tudo começou com a iniciativa das meninas do curso de <strong>Engenharia de
-        Software do iCEV</strong>, que, com o apoio da coordenação da faculdade,
-        decidiram se unir para se apoiar e crescer juntas na área de tecnologia.
-        A partir dessa união, surgiu a ideia de <strong>propagar educação gratuita e
-        acessível</strong>, incentivando mais mulheres a explorarem o universo da
-        inovação. Somos uma comunidade aberta, onde <strong>mulheres podem participar e
-        contribuir voluntariamente</strong>, compartilhando conhecimento e inspirando
-        outras pessoas. Criamos conteúdos de <strong>mulheres para todos</strong>, acreditando
-        que a tecnologia deve ser acessível e inclusiva para qualquer pessoa
-        interessada em aprender. Atuamos em três áreas principais: <strong>Inteligência
-        Artificial, Cibersegurança e Experiência do Usuário (UX)</strong>. Acreditamos
-        que o acesso à informação e o aprendizado colaborativo são essenciais
-        para transformar o mercado de tecnologia. Nosso site oferece conteúdos
-        educativos por meio de um <strong>blog</strong> com artigos informativos, um <strong>fórum aberto</strong> 
-        para dúvidas e discussões e uma área de <strong>mini cursos gratuitos com
-        certificado</strong>, para quem deseja aprender e se especializar.
+        Tudo começou com a iniciativa das meninas do curso de{" "}
+        <strong>Engenharia de Software do iCEV</strong>, que, com o apoio da
+        coordenação da faculdade, decidiram se unir para se apoiar e crescer
+        juntas na área de tecnologia. A partir dessa união, surgiu a ideia de{" "}
+        <strong>propagar educação gratuita e acessível</strong>, incentivando
+        mais mulheres a explorarem o universo da inovação. Somos uma comunidade
+        aberta, onde{" "}
+        <strong>mulheres podem participar e contribuir voluntariamente</strong>,
+        compartilhando conhecimento e inspirando outras pessoas. Criamos
+        conteúdos de <strong>mulheres para todos</strong>, acreditando que a
+        tecnologia deve ser acessível e inclusiva para qualquer pessoa
+        interessada em aprender. Atuamos em três áreas principais:{" "}
+        <strong>
+          Inteligência Artificial, Cibersegurança e Experiência do Usuário (UX)
+        </strong>
+        . Acreditamos que o acesso à informação e o aprendizado colaborativo são
+        essenciais para transformar o mercado de tecnologia. Nosso site oferece
+        conteúdos educativos por meio de um <strong>blog</strong> com artigos
+        informativos, um <strong>fórum aberto</strong>
+        para dúvidas e discussões e uma área de{" "}
+        <strong>mini cursos gratuitos com certificado</strong>, para quem deseja
+        aprender e se especializar.
       </Text>
 
       <SimpleGrid cols={{ base: 1, md: 3 }} spacing="md" mt={50}>
